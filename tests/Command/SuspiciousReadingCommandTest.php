ef661457ca86aba5247b4bbe0474a758c4bd27dc
xevi <xevi@brainityapp.com>
2021-01-25 09:24:23 +0100

    Starter template

---
+++ tests/Command/SuspiciousReadingCommandTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Command;
+
+use App\Command\SuspiciousReadingCommand;
+use PHPUnit\Framework\TestCase;
+
+class SuspiciousReadingCommandTest extends TestCase
+{
+
+    public function testBasic() {
+        $this->assertEquals('1','1');
+    }
+}
